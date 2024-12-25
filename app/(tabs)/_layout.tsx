5839007328721ece86b8d3dc8154f157bcd77770
Mohamed Ahmed <mohamed_ali98@hotmail.co.uk>
2024-12-25 00:18:33 +0000

    ref: add blur affect on tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,11 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { Platform, StyleSheet } from "react-native";
 
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { BlurView } from "expo-blur";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -13,14 +15,25 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tabIconSelected,
         headerShown: false,
+        tabBarBackground: () => (
+          <BlurView
+            tint={colorScheme === "dark" ? "dark" : "light"}
+            intensity={Platform.OS === "ios" ? 25 : 100}
+            style={StyleSheet.absoluteFill}
+          />
+        ),
         tabBarStyle: {
-          backgroundColor: Colors[colorScheme ?? "light"].background,
+          position: "absolute",
+          backgroundColor: Colors[colorScheme ?? "light"].background + "CC",
           borderTopWidth: 0,
           elevation: 4,
           shadowColor: "#000",
           shadowOffset: { width: 0, height: -1 },
           shadowOpacity: 0.1,
           shadowRadius: 4,
+          bottom: 0,
+          left: 0,
+          right: 0,
         },
       }}
     >
